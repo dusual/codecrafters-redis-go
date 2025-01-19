bf249c057a595ba24fc021ca394e69c450d70ed1
Amit Singh Sethi <amit.pureenergy@gmail.com>
2025-01-19 20:09:11 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
@@ -21,9 +18,28 @@ func main() {
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	for {
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	// Read data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(buf))
 	conn.Write([]byte("+PONG\r\n"))
 }
