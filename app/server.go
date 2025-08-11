90bcd0751d2eac1abd51da54a64d2120e03d551e
Amit Singh Sethi <amit.pureenergy@gmail.com>
2025-08-11 11:20:07 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,10 +1,66 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func parseRespCommand(reader *bufio.Reader) ([]string, error) {
+	line, err := reader.ReadString('\n')
+
+	if err != nil {
+		return nil, err
+	}
+	line = strings.TrimRight(line, "\r\n")
+	switch {
+	case strings.HasPrefix(line, "*"):
+		numElems, _ := strconv.Atoi(line[1:])
+		parts := make([]string, 0, numElems)
+		raiseError := false
+		errorMessage := ""
+		for i := 0; i < numElems; i++ {
+			// read $len
+
+			lenLine, _ := reader.ReadString('\n')
+			lenLine = strings.TrimRight(lenLine, "\r\n")
+
+			if !strings.HasPrefix(lenLine, "$") {
+				fmt.Println("unexpected bulk string header:", lenLine)
+				raiseError = true
+				errorMessage = "unexpected bulk string header: " + lenLine
+				break
+
+			}
+			strLen, _ := strconv.Atoi(lenLine[1:])
+
+			// read actual string
+			buf := make([]byte, strLen+2)
+			_, err := io.ReadFull(reader, buf)
+			if err != nil {
+				raiseError = true
+				errorMessage = err.Error()
+				break
+			}
+			parts = append(parts, string(buf[:strLen]))
+		}
+		if raiseError {
+			return nil, errors.New(errorMessage)
+		}
+		return parts, nil
+	default:
+		return nil, errors.New("Could not parse bulk String")
+	}
+}
+
+func writeBulkString(w io.Writer, s string) {
+	fmt.Fprintf(w, "$%d\r\n%s\r\n", len(s), s)
+}
 
 func acceptConns(l net.Listener, acceptChan chan net.Conn) {
 	// Need a for loop to handle continuously handle connections
@@ -48,12 +104,27 @@ func handleConn(conn net.Conn) {
 	// Read data
 	defer conn.Close()
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		reader := bufio.NewReader(conn)
+		//cmd, err := parseRespCommand(reader)
+		cmdParts, err := parseRespCommand(reader)
 		if err != nil {
+			if err != io.EOF {
+				fmt.Println("read error:", err)
+			}
 			return
 		}
-		fmt.Println(string(buf))
-		conn.Write([]byte("+PONG\r\n"))
+		switch strings.ToUpper(cmdParts[0]) {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			if len(cmdParts) < 2 {
+				conn.Write([]byte("-ERR wrong number of arguments for 'echo' command\r\n"))
+				continue
+			}
+			writeBulkString(conn, cmdParts[1])
+		default:
+			conn.Write([]byte("-ERR unknown command\r\n"))
+		}
 	}
+	fmt.Println("Connection closed")
 }
